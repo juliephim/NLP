66e678fa29c5111999fa92ccea49cc833179fca1
juliephim <118478365+juliephim@users.noreply.github.com>
2023-10-22 15:45:22 +0200

    final

--- tokenization_app.py
+++ tokenization_app.py
@@ -4,7 +4,9 @@ import nltk
 from nltk.tokenize import word_tokenize, RegexpTokenizer, TreebankWordTokenizer, WordPunctTokenizer
 from nltk.corpus import stopwords
 import re
-
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.metrics import jaccard_distance
+nltk.download('wordnet')  # Assurez-vous d'avoir le corpus WordNet téléchargé
 
 replacement_patterns = [
 	(r'won\'t', 'will not'),
@@ -69,7 +71,21 @@ def remove_stopwords(text, language='english'):
     return [word for word in words if word not in stops]
 
 def main():
-    st.title("Tokenization avec NLTK")
+
+    # Barre latérale de navigation
+    option = st.sidebar.selectbox(
+    ('Tokenization', 'Onglet 2', 'Onglet 3'))
+
+    if option == 'Tokenization':
+        show_tokenization()
+    elif option == 'Onglet 2':
+        show_lemmatization()
+    else:
+        Similarity()
+
+#-----------------------------1er ONGLET : TOKENIZATION WITH NLTK
+def show_tokenization():
+    st.title("Tokenization with NLTK")
     
     # Saisie de texte par l'utilisateur
     user_input = st.text_area("Enter the text to tokenize:", "")
@@ -106,6 +122,45 @@ def main():
             st.write(tokenize_with_regex(substitution(user_input)))
         elif method == "Removing stop words":
             st.write(remove_stopwords(substitution(user_input),'english'))
+
+#-----------------------------2eme ONGLET : Lemmatization & Stemming
+def show_lemmatization():
+    st.write("Lemmatization & Stemming")
+
+    user_input = st.text_area("Enter the text here:")
+
+    processing_option = st.selectbox(
+        'Choose an option:',
+        ('Lemmatization', 'Stemming')
+    )
+
+    if st.button('Validate'):
+        if processing_option == 'Lemmatization':
+            lemmatizer = WordNetLemmatizer()
+            result = ' '.join([lemmatizer.lemmatize(word) for word in user_input.split()])
+            st.write("Result after Lemmatization:", result)
+        else:
+            stemmer = PorterStemmer()
+            result = ' '.join([stemmer.stem(word) for word in user_input.split()])
+            st.write("Result after Stemming:", result)
+
+#-----------------------------3eme ONGLET : Similarity_measure
+def Similarity():
+    st.write("Similarity measurement using Jaccard's coefficient")
+
+    # Les zones de texte pour entrer les séquences de mots
+    seq1 = st.text_area("Enter the first sequence of words (separated by spaces):")
+    seq2 = st.text_area("Enter the second sequence of words (separated by spaces):")
+
+    if st.button('Calculate Jaccard distance'):
+        # Convertir les entrées en ensembles de mots
+        X = set(seq1.split())
+        Y = set(seq2.split())
+
+        # Calculer la distance de Jaccard
+        distance = jaccard_distance(X, Y)
+        st.write(f"Jaccard Distance: {distance:.2f}")
+
 
 if __name__ == "__main__":
     main()
